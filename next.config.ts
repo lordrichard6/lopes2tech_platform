6a0ef201b4b9424da1f13ed9bb8408094cd84324
Paulo R Lopes Reizinho <paulolopes@Paulos-MacBook-Pro.local>
2026-01-28 12:01:56 +0100

    fix: add global CORS headers for external API

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,20 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        // Aleady matching the /api/external route
+        source: "/api/external/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace with specific domain if needed
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, x-api-key" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
